a0fa5d51416f702be35209341ce1ee77e0a18cbd
mkut <m.k.u.t.at@gmail.com>
2012-05-28 00:42:58 +0900

    003B

---
+++ 003/B/haskell.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE Rank2Types #-}
+import Data.List
+import qualified Data.ByteString.Lazy.Char8 as C
+import System.IO
+import Text.Parsec.Prim
+import Text.Parsec.Combinator
+import Text.Parsec.Char
+import Text.Printf
+import Data.Functor.Identity
+import Control.Monad
+
+main = contestMain printer solver parser
+
+solver = map reverse . sort . map reverse
+
+printer h = hPutStr h . unlines
+
+parser = do
+   n <- number
+   count n word
+
+-- Milib.Contest
+type Printer a = Handle -> a -> IO ()
+type Solver a b = a -> b
+type Parser b = Stream C.ByteString Identity Char => Parsec C.ByteString () b
+type CMain a b = Printer b -> Solver a b -> Parser a -> IO ()
+type HCMain a b = Handle -> Handle -> CMain a b
+
+instance Stream C.ByteString Identity Char where
+   uncons = return . C.uncons
+
+hContestMain :: HCMain a b
+hContestMain hin hout printer solver parser = do
+   input <- C.hGetContents hin
+   case parse parser "" input of
+      Left err -> do { hPutStr stderr "parse err: "; hPrint stderr err }
+      Right x  -> printer hout $ solver x
+
+contestMain :: CMain a b
+contestMain = hContestMain stdin stdout
+
+-- Milib.IO
+number' :: (Stream s m Char, Integral a, Read a) => ParsecT s u m a
+number' =
+       do ds <- many1 digit
+          return (read ds)
+   <?> "number"
+
+number :: (Stream s m Char, Integral a, Read a) => ParsecT s u m a
+number = do spaces; number'
+
+word' :: Stream s m Char => ParsecT s u m String
+word' = many1 letter
+
+word :: Stream s m Char => ParsecT s u m String
+word = do spaces; word'
+
+-- vim: set expandtab:
