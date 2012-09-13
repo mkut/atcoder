9d76e2b7f0af057e8ee1e31500c4668a3af983ac
mkut <m.k.u.t.at@gmail.com>
2012-09-13 18:22:06 +0900

    arc007/B

---
+++ arc007/B/haskell.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE Rank2Types #-}
+import qualified Data.ByteString.Lazy.Char8 as C
+import System.IO
+import Text.Parsec.Prim
+import Text.Parsec.Combinator
+import Text.Parsec.Char
+import Text.Printf
+import Data.Functor.Identity
+import Control.Monad
+
+main = contestMain printer solver parser
+
+solver (n, disk) = snd $ foldl f (0, [1..n]) disk
+   where
+      f (x, xs) y = (y, map g xs)
+         where
+            g z = if z == y then x else z
+
+printer h = mapM_ (hPrint h)
+
+parser = do
+   n <- number
+   m <- number
+   disk <- count m number
+   return (n, disk)
+
+-- Milib.Contest
+type Printer a = Handle -> a -> IO ()
+type Solver a b = a -> b
+type Parser b = Stream C.ByteString Identity Char => Parsec C.ByteString () b
+type CMain a b = Printer b -> Solver a b -> Parser a -> IO ()
+type HCMain a b = Handle -> Handle -> CMain a b
+
+instance Stream C.ByteString Identity Char where
+   uncons = return . C.uncons
+
+hContestMain :: HCMain a b
+hContestMain hin hout printer solver parser = do
+   input <- C.hGetContents hin
+   case parse parser "" input of
+      Left err -> do { hPutStr stderr "parse err: "; hPrint stderr err }
+      Right x  -> printer hout $ solver x
+
+contestMain :: CMain a b
+contestMain = hContestMain stdin stdout
+
+-- Milib.IO
+number' :: (Stream s m Char, Integral a, Read a) => ParsecT s u m a
+number' =
+       do ds <- many1 digit
+          return (read ds)
+   <?> "number"
+
+number :: (Stream s m Char, Integral a, Read a) => ParsecT s u m a
+number = do spaces; number'
+
+-- vim: set expandtab:
